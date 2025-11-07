3cb3a66e72c658db1c747863d9cd19f1e8da5c88
Richard <rcezea007@gmail.com>
2025-11-07 11:37:16 +0100

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Library, Book
 
 # Create your views here.
