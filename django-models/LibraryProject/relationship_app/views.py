1df94a3c7c613a1221f1d45421a0fd58fd338b01
Richard <rcezea007@gmail.com>
2025-11-07 11:35:52 +0100

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book, Library
 
 # Create your views here.
+
+def list_books(request):
+    book_list = Book.objects.all()
+    context = {
+        "books": book_list
+    }
+    return render(request, template_name="relationship_app/list_books.html", status=200, context=context)
+
+class LibraryDetail(ListView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_queryset(self):
+        return Library.objects.first()
