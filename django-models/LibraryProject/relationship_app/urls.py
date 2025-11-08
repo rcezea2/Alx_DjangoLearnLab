5d07dfc6ea0f7633c995f279a546d01f0ef14b03
Richard <rcezea007@gmail.com>
2025-11-08 23:06:58 +0100

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetail, RegisterView, Login, Logout, home
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path("books/list_books/", list_books, name="list_books"),
     path("books/LibraryDetailView/", LibraryDetail.as_view(), name="library_detail"),
     path("register", RegisterView.as_view(), name="register"),
-    path("login", Login.as_view(), name="login"),
-    path("logout", Logout.as_view(), name="logout"),
+    path("login", LoginView.as_view(template_name="relationship_app/login.html", next_page="dashboard"), name="login"),
+    path("logout", Logout.as_view(template_name = "relationship_app/logout.html"), name="logout"),
     path("home", home, name="dashboard")
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
-from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 from django.views.generic.detail import DetailView
 from .models import Library, Book
@@ -26,14 +25,14 @@ class LibraryDetail(ListView):
     def get_queryset(self):
         return Library.objects.first()
 
-class Login(LoginView):
-    form_class = AuthenticationForm
-    template_name = "relationship_app/login.html"
-    # next_page = "https://www.google.com"
-    next_page = "dashboard"
+# class Login(LoginView):
+#     form_class = AuthenticationForm
+#     template_name = "relationship_app/login.html"
+#     # next_page = "https://www.google.com"
+#     next_page = "dashboard"
 
-class Logout(LogoutView):
-    template_name = "relationship_app/logout.html"
+# class Logout(LogoutView):
+#     template_name = "relationship_app/logout.html"
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
