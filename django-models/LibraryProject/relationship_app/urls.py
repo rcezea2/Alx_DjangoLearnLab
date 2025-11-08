b39910044d2b1a18df2f1ad321760a7b3ee72742
Richard <rcezea007@gmail.com>
2025-11-08 23:00:00 +0100

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetail
+from .views import list_books, LibraryDetail, RegisterView, Login, Logout, home
 
 urlpatterns = [
-    path("list_books/", list_books, name="list_books"),
-    path("LibraryDetailView/", LibraryDetail.as_view(), name="library_detail")
+    path("books/list_books/", list_books, name="list_books"),
+    path("books/LibraryDetailView/", LibraryDetail.as_view(), name="library_detail"),
+    path("register", RegisterView.as_view(), name="register"),
+    path("login", Login.as_view(), name="login"),
+    path("logout", Logout.as_view(), name="logout"),
+    path("home", home, name="dashboard")
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 
@@ -19,3 +24,21 @@ class LibraryDetail(ListView):
 
     def get_queryset(self):
         return Library.objects.first()
+
+class Login(LoginView):
+    form_class = AuthenticationForm
+    template_name = "relationship_app/login.html"
+    # next_page = "https://www.google.com"
+    next_page = "dashboard"
+
+class Logout(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "relationship_app/register.html"
+    success_url = reverse_lazy('login')
+
+@login_required()
+def home(request):
+    return render(request, "relationship_app/home.html")
