8e441f912ba0324756bb146ba08a63e1271aad16
Richard <rcezea007@gmail.com>
2025-11-16 23:14:38 +0100

    2. Implementing Security Best Practices in Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,15 @@
+# bookshelf/forms.py
+from django import forms
+from .models import Book  # Import your Book model
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+
+    # Optional: Add custom validation if needed
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 5:
+            raise forms.ValidationError("Title must be at least 5 characters long.")
+        return title
