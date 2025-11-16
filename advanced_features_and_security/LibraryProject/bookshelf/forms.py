02bb4cfc6851af6f12204e8f53e1af2ba5858067
Richard <rcezea007@gmail.com>
2025-11-16 23:15:03 +0100

    2. Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from .models import Book  # Import your Book model
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
